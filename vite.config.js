12ff076983b40c79bc06b3e7a3695efd9bf10df5
bradbbang <bradbbang@gmail.com>
2025-04-13 21:57:11 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({ plugins: [react()] })
+export default defineConfig({
+  plugins: [react()],
+  root: '.',                // 프로젝트 루트 위치
+  build: {
+    outDir: 'dist',         // 결과물을 저장할 폴더
+  },
+})
